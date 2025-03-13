51758db1e30e7e1f965f00b6531be818923168ed
s33498 <s33498@pjwstk.edu.pl>
2025-03-13 11:00:22 +0100

    Initial project

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-
+    public static int[] filterEvenIndexOddValue(int[] array){
+    int counter = 0;
+    for(int i = 0; i < array.length; i++){
+        if(array[i] % 2 == 0){
+            counter++;
+        }
+    int[] result = new int[counter];
+        for(int j = 0; j < counter; j++){
+            result[j] = array[i];
+        }
+    return result;
+    }
     }
 }
