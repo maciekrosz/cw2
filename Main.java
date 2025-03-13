c8731a8eb8fe4830a0af120c9ccc45f77c745e6c
s33498 <s33498@pjwstk.edu.pl>
2025-03-13 11:21:52 +0100

    z2Poprawne

--- Main.java
+++ Main.java
@@ -20,6 +20,6 @@ public class Main {
     }
     public static void main(String[] args) {
         int[] array = {3, 3, 3, 2, 2};
-        System.out.println(findDominant);
+        System.out.println(findDominant(array));
     }
 }
