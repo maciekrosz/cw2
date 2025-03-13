03c029f1100c67e2064f83c14e2a543315d0d27e
s33498 <s33498@pjwstk.edu.pl>
2025-03-13 11:08:21 +0100

    z1

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
-public class Main {
-    public static int[] filterEvenIndexOddValue(int[] array){
-    int counter = 0;
-    for(int i = 0; i < array.length; i++){
-        if(array[i] % 2 == 0){
-            counter++;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public static int[] filterEvenIndexOddValue(int[] array) {
+    int count = 0;
+    for (int i = 0; i < array.length; i++) {
+        if (i % 2 == 0 && array[i] % 2 != 0) {
+            count++;
         }
-    int[] result = new int[counter];
-        for(int j = 0; j < counter; j++){
-            result[j] = array[i];
+    }
+    int[] result = new int[count];
+    int index = 0;
+    for (int i = 0; i < array.length; i++) {
+        if (i % 2 == 0 && array[i] % 2 != 0) {
+            result[index++] = array[i];
         }
+    }
     return result;
-    }
-    }
+}
+
+public void main() {
+    int[] array = new int[]{1, 2, 3, 4, 5, 6};
+    int[] result = filterEvenIndexOddValue(array);
+    System.out.println(Arrays.toString(result));
 }
